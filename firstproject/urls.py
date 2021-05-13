3519889a434e3d9658a23b2c11e64da0700bca5d
Susmita <smileysusmita2055@gmail.com>
2021-05-13 17:09:49 +0545

    created new blog app

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from blog import urls
+
 from django.contrib import admin
 from django.urls import path, include
-from django.urls.conf import include
+from blog import views
+
 
 urlpatterns = [
-    
-    path('',include('blog.urls')),
+    path('blog/',include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('',views.home),
 ]
