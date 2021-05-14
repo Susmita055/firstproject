1841717f33c7f470a8106ed017f8cbb19ce35c80
Susmita <smileysusmita2055@gmail.com>
2021-05-14 20:34:35 +0545

    My first blog

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 # Create your views here.
 def post_list(request):
     return render(request,'post_list.html', {})
-
+def post_list(request):
+    posts= Post.objects.all()
+    return render(request, 'post_list.html',{'posts': posts})
 def home(request):
     return HttpResponse("HELLO!!!")
 
