afc65773c9e5c666577aa970daeb3c1dd2394f1d
Susmita <smileysusmita2055@gmail.com>
2021-05-18 20:19:40 +0545

    My first blog

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render,redirect, get_object_or_404
 from django.http import HttpResponse
+from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 def post_list(request):
     return render(request,'post_list.html', {})
@@ -12,6 +14,21 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'post_detail.html', {'post':post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_list')
+    else:
+        form= PostForm()
+    return render(request, 'post_new.html',{'form':form})
+
+
 
 def home(request):
     return HttpResponse("HELLO!!!")
