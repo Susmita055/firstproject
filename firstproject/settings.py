536cfe629138cac7152810cf16fbe51e49dec2fb
Susmita <smileysusmita2055@gmail.com>
2021-05-29 17:23:38 +0545

    version

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -40,8 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    
 
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -131,8 +133,8 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'smileysusmita2055@gmail.com'
-EMAIL_HOST_PASSWORD = 'Smiley@@0231'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 # Default primary key field type
